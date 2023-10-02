dhruv/bitcoin-core-ci

rpc: addnode arg to use BIP324 v2 p2p

src/rpc/client.cpp
@@ -201,6 +201,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "addpeeraddress", 1, "port"},
     { "addpeeraddress", 2, "tried"},
     { "stop", 0, "wait" },
+    { "addnode", 2, "p2p_v2" },
 };
 // clang-format on
 

BigOunceChristo/bitpulse

Co-authored-by: Pieter Wuille <bitcoin-dev@wuille.net>

src/rpc/client.cpp
@@ -301,6 +301,7 @@ static const CRPCConvertParam vRPCConvertParams[] =
     { "addpeeraddress", 2, "tried"},
     { "sendmsgtopeer", 0, "peer_id" },
     { "stop", 0, "wait" },
+    { "addnode", 2, "v2transport" },
 };
 // clang-format on
 